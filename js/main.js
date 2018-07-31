445f1b803abfcf244cfb93647d5fa1923f4887f4
MizoPro <hamza.randy@gmail.com>
2018-07-31 18:37:58 +0100

    Initialize styles and scripts

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*!
+ * 
+ * MyPortfolio.JS
+ * 
+ * Author: MizoPro
+ * 
+ * Licensed under the MIT License
+ * All rights are reserved®
+ * 
+ */
+
+/**
+ * @const {Object} MP MizoPro's public info and URLs
+ * 
+ */ 
+const MP = window.MP = window.MP || {};
+
+window.MP.nickname = "MizoPro";
+window.MP.firstName = "Hamza";
+window.MP.lastName = "Belhadj Ahmed";
+window.MP.social = {
+    orcid:    'https://orcid.org/0000-0001-9668-9225',
+    github:   'https://github.com/MizoPro', 
+    twitter:  'https://twitter.com/MizoPro', 
+    linkedin: 'https://linkedin/in/mizopro'
+};
+
+/**
+ * Main entry
+ * 
+ */
+$(document).ready(function() {
+    //NULL
+});
